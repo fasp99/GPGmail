be105df7e6950d912ee0ceffd842c6444e4587da
fasp99 <fasp99@gmail.com>
2015-01-03 16:00:41 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -13,8 +13,12 @@ var main = function(){
 // NOTE: Always use the latest version of gmail.js from
 // https://github.com/KartikTalwar/gmail.js
 	gmail = new Gmail();
-	console.log('Hello,', gmail.get.user_email())
-	alert('Hello,', gmail.get.user_email());
+	var str = 'Hello, ';
+	var email = gmail.get.user_email();
+	var toast = str.concat(email);
+	console.log(toast)
+	//console.log('Hello, ', gmail.get.user_email())
+	alert(toast);
 }
 
 refresh(main);
@@ -31,6 +35,8 @@ hashCheck();
 function mailOpened() {
 gmail = new Gmail();
 var subject = gmail.get.email_subject();
-	alert(subject);
-	console.log('Subject: ', subject)
+var str = "Subject: ";
+var toast = str.concat(subject);
+alert(toast);
+console.log(toast)
 }
